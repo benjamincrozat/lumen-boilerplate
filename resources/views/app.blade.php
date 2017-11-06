ac0839b4aa629a61796467b740fefc4837ddadb2
Benjamin Crozat <benjamin@croz.at>
2017-11-06 08:30:56 +0100

    Added Laravel Mix in case of basic styling / JS needs

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,10 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Lumen Boilerplate</title>
+
+        <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
         <div id="app">
             <h1>{{ app()->version() }}</h1>
         </div>
+
+        <script src="/js/app.js"></script>
     </body>
 </html>
