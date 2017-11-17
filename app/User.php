4d0620db94b148f8ac971002e00fd75be4ce3a26
Benjamin Crozat <benjamin@croz.at>
2017-11-17 21:49:02 +0100

    Clean up

--- app/User.php
+++ app/User.php
@@ -2,7 +2,7 @@
 
 namespace App;
 
-use Laravel\Passport\HasApiTokens;
+// use Laravel\Passport\HasApiTokens;
 use Illuminate\Auth\Authenticatable;
 use Laravel\Lumen\Auth\Authorizable;
 use Illuminate\Database\Eloquent\Model;
@@ -11,7 +11,10 @@ use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
 class User extends Model implements AuthenticatableContract, AuthorizableContract
 {
-    use Authenticatable, Authorizable, HasApiTokens;
+    use Authenticatable,
+        Authorizable,
+        // HasApiTokens
+    ;
 
     /**
      * The attributes that are mass assignable.
