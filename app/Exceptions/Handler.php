8b432c6b61a05fd279445445ec32230c7449ed49
Benjamin Crozat <benjamin@croz.at>
2017-11-21 15:00:06 +0100

    Forgot to import namespace

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
