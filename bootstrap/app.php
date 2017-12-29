8d5890f416370d32d63502ce5335f2cf745ecf64
Benjamin Crozat <benjamin@croz.at>
2017-12-29 20:01:39 +0100

    Clean up

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -93,7 +93,6 @@ $app->routeMiddleware([
 $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(App\Providers\EventServiceProvider::class);
-
 $app->register(Illuminate\Redis\RedisServiceProvider::class);
 
 /*
