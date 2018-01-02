bb67b4236a7eb4f5d3b548350affdb7d9b2e27bd
Benjamin Crozat <benjamin@croz.at>
2018-01-02 10:06:21 +0100

    Removed "remember_token" from migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,6 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->string('password');
             $table->string('api_token', 60);
-            $table->rememberToken();
             $table->timestamps();
         });
     }
