be1e9ae4cad45781b562bc1db67c65e9db141af2
Benjamin Crozat <benjamin@croz.at>
2018-01-03 21:32:26 +0100

    Testing throttling

---
+++ tests/Integration/ThrottleRequestsTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Carbon;
+
+/**
+ * @group integration
+ */
+class ThrottleRequestsTest extends TestCase
+{
+    /** @test */
+    public function lock_opens_immediately_after_decay()
+    {
+        Carbon::setTestNow(null);
+
+        app('router')->get('/', ['middleware' => 'throttle:2,1']);
+
+        $this->json('GET', '/')
+            ->seeHeader('X-RateLimit-Limit', 2)
+            ->seeHeader('X-RateLimit-Remaining', 1);
+
+        $this->json('GET', '/')
+            ->seeHeader('X-RateLimit-Limit', 2)
+            ->seeHeader('X-RateLimit-Remaining', 0);
+
+        Carbon::setTestNow(
+            Carbon::now()->addSeconds(58)
+        );
+
+        $this->json('GET', '/')
+            ->seeStatusCode(429);
+    }
+}
