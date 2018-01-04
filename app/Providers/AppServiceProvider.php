192da091359fe13f078aa425ea6c6b79bc7e8a60
Benjamin Crozat <benjamin@croz.at>
2018-01-04 15:31:11 +0100

    Using the contract as binding

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Repositories\PostsRepository;
 use Illuminate\Support\ServiceProvider;
+use App\Contracts\PostsRepositoryContract;
 use App\Repositories\PostsCacheRepository;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(PostsCacheRepository::class, function () {
+        $this->app->bind(PostsRepositoryContract::class, function () {
             return new PostsCacheRepository(new PostsRepository());
         });
     }
