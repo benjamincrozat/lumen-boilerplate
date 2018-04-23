dab0853ca609483993763a3ad6ce1c77f7ea6438
Benjamin Crozat <benjamin@croz.at>
2018-04-23 15:08:42 +0200

    Added IDE helper

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -107,6 +107,7 @@ $app->register(Laravel\Tinker\TinkerServiceProvider::class);
 $app->register(Illuminate\Redis\RedisServiceProvider::class);
 
 if (! $app->environment('production')) {
+    $app->register(Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
     $app->register(Clockwork\Support\Lumen\ClockworkServiceProvider::class);
 }
 
